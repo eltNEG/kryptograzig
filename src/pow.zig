ac2e708a0ee4853ee40caa33447645f03a8823d5
eltneg <yorewole@gmail.com>
2024-11-24 22:23:48 +0000

    Do mpow as bit ops

--- src/pow.zig
+++ src/pow.zig
@@ -6,10 +6,18 @@ pub fn pow(comptime T: type, b: T, e: T) T {
     return r;
 }
 pub fn mpow(comptime T: type, b: T, e: T, m: T) T {
+    var n = e;
+    var a = b;
     var r: T = 1;
-    for (e) |_| {
-        r *= b;
-        r = r % m;
+    while (n > 0) {
+        if (n & 1 > 0) {
+            r *= a;
+            r %= m;
+        }
+        a *= a;
+        a %= m;
+
+        n = n >> 1;
     }
     return r;
 }
@@ -19,4 +27,5 @@ test "pow" {
     try std.testing.expect(pow(u64, 2, 2) == 4);
     try std.testing.expect(pow(u64, 4, 2) == 16);
     try std.testing.expect(mpow(u64, 4, 2, 17) == 16);
+    try std.testing.expect(mpow(u64, 3, 255, 999) == 27);
 }
